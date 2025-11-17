097d643c59cc9304e0169f83e467eefd41c1d24f
yangyekun <ahwater@321>
2025-11-17 15:20:15 +0800

    .

--- src/main.js
+++ src/main.js
@@ -7,10 +7,7 @@ import 'primeicons/primeicons.css'
 import ToastService from 'primevue/toastservice';
 import ConfirmationService from 'primevue/confirmationservice'; 
 import zh_cn from './assets/zh_cn.js';
-// import OrpType from './components/orpType.vue'
-// import OrpStatus from './components/orpStatus.vue'
-// import OrpButton from './components/orpButton.vue'
-// import OrpMsg from './components/orpMsg.vue'
+
 import OrpActions from '@/components/orpActions.vue'
 import BtnActions from '@/components/btnActions.vue'
 import './utils/ag-grid.js'
@@ -21,13 +18,11 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const app = createApp(App)
+
 app.use(router)
-// app.component('OrpType', OrpType)
-// app.component('OrpStatus', OrpStatus)
-// app.component('OrpButton', OrpButton)
-// app.component('OrpMsg', OrpMsg)
 app.component('OrpActions', OrpActions)
 app.component('BtnActions', BtnActions)
+
 app.use(PrimeVue, {
   theme: {
     preset: themePreset,
